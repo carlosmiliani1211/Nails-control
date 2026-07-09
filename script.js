2bed669468b864055710cf8aaa69525e4df4b57d
carlosmiliani1211 <carlosmiliani1211@gmail.com>
2026-07-09 01:54:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ const preciosServicios = {
     "Poly Gel": 38000,
     "Manicure": 11000,
     "Pedicure": 18000
+    "Retiro Sencillo": 5000
+    "Retiro Gel o Acrilico": 10000
 
 };
 function mostrarFormulario(){
